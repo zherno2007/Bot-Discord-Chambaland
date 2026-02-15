1aa79114f80a94bb02d9ead23fb3b2015e9ada9a
zherno2007 <zherno2007@gmail.com>
2026-02-15 10:54:52 -0500

    Update bot.py
    simplemente cambio de id de verificacion

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 # ========= CONFIGURACIÓN =========
 TOKEN = os.getenv("TOKEN")
 
-CANAL_VERIFICACION_ID = 1471608546620739604  
+CANAL_VERIFICACION_ID = 1472621737786212434  
 CANAL_LOGS_ID = 1471656681195966586          
 
 ROL_VERIFICADO_ID = 1471637465700892673
@@ -172,4 +172,5 @@ async def panel(ctx):
     await ctx.message.delete()
 
 bot.run(TOKEN)
+
 
